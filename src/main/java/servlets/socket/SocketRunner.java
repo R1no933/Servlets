22acb5b1edcb13e22c4e1567f830496d9a3031e5
R1no933 <dbaskakov93@yandex.ru>
2023-09-23 20:19:38 +0300

    TCP. Socket server example

--- src/main/java/servlets/socket/SocketRunner.java
+++ src/main/java/servlets/socket/SocketRunner.java
@@ -4,21 +4,24 @@ import java.io.*;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
         // http port - 80
         // https port - 443
-        InetAddress inetAddress = Inet4Address.getByName("google.com");
+        InetAddress inetAddress = Inet4Address.getByName("localhost");
 
-        try (Socket socket = new Socket(inetAddress, 80);
+        try (Socket socket = new Socket(inetAddress, 7777);
              DataOutputStream  outputStream = new DataOutputStream(socket.getOutputStream());
-             DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
 
-            outputStream.writeUTF("Hello world!!!");
-            byte[] allBytesResponce = inputStream.readAllBytes();
-
-            System.out.println(allBytesResponce.length);
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Responce from server is: " + inputStream.readUTF());
+            }
         }
     }
 }
