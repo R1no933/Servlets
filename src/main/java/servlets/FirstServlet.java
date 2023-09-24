03b7ca7b4dd4c8a7141cd00018ff175a1b577033
R1no933 <dbaskakov93@yandex.ru>
2023-09-24 20:50:27 +0300

    SERVLETS. Work with headers. Create custom header and get all headers

--- src/main/java/servlets/FirstServlet.java
+++ src/main/java/servlets/FirstServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 @WebServlet("/firstServlet")
 public class FirstServlet extends HttpServlet {
@@ -20,7 +21,14 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String header = headerNames.nextElement();
+            System.out.println(req.getHeader(header));
+        }
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "1234567");
         try (PrintWriter writer = resp.getWriter()) {
             writer.write("Hello from first servlet");
         }
