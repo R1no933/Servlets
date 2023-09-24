43f4f1924ab45d66d124c40504f6546c3e25c635
R1no933 <dbaskakov93@yandex.ru>
2023-09-24 21:23:36 +0300

    SERVLETS Work with req body and read it from req

--- src/main/java/servlets/FirstServlet.java
+++ src/main/java/servlets/FirstServlet.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/firstServlet")
 public class FirstServlet extends HttpServlet {
@@ -41,8 +43,12 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        System.out.println(parameterMap);
+        try (BufferedReader reader = req.getReader();
+             Stream<String> lines = reader.lines()) {
+            lines.forEach(System.out::println);
+        }
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+//        System.out.println(parameterMap);
     }
 
     @Override
