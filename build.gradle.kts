0046af32d52e3befae418e9614b35c665f5cdb84
R1no933 <dbaskakov93@yandex.ru>
2023-09-24 19:44:32 +0300

    WAR. Config project for web app files generation, and add test index.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,24 @@
 plugins {
     id("java")
+    war
 }
 
 group = "servlets"
-version = "1.0-SNAPSHOT"
+version = ""
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    providedCompile("javax.servlet:servlet-api:2.5")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.war {
+    webAppDirectory.set(file("src/main/webapp"))
 }
 
 tasks.test {
