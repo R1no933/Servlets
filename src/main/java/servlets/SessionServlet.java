813a3fe512cf58a302e7b175916a3f335876f047
R1no933 <dbaskakov93@yandex.ru>
2023-09-28 22:38:03 +0300

    MVC. Working with dispatcher

---
+++ src/main/java/servlets/SessionServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import servlets.dto.UserDto;
+
+import java.io.IOException;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+
+    private static final String USER = "user";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object user = (UserDto) session.getAttribute(USER);
+
+        if (user == null) {
+            user = UserDto.builder()
+                    .id(19L)
+                    .email("test@mail.ru")
+                    .build();
+        }
+
+        session.setAttribute(USER, user);
+
+    }
+}
