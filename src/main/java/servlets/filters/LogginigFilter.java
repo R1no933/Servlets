04e572b3e31d319219485c13336441079e8ce255
R1no933 <dbaskakov93@yandex.ru>
2023-09-30 17:59:10 +0300

    SERVLET FILTER. Work with servlet filters

---
+++ src/main/java/servlets/filters/LogginigFilter.java
@@ -0,0 +1,16 @@
+package servlets.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebFilter("/*")
+public class LogginigFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.getParameterMap().forEach((k, v) -> System.out.println(k + ": " + Arrays.toString(v)));
+        chain.doFilter(request, response);
+    }
+}
